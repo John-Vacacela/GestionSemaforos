02f66ac69716f975c4cc3b60f6a7298152b3d0e1
John Vacacela <john.vacacela@ucuenca.edu.ec>
2024-10-04 22:45:57 -0500

    Vista

--- vista/vistaCalles.py
+++ vista/vistaCalles.py
@@ -23,16 +23,30 @@ class vistaCalles(QWidget):
     def paintEvent(self, event):
         painter = QPainter(self)
         self.drawCalle(painter)
+        self.drawSemaforo(painter)
         painter.end()
 
     def drawCalle(self, painter):
         painter.setPen(Qt.NoPen)
         # Dibujar las calles (rectángulos grises)
-        painter.setBrush(QBrush(QColor(128, 128, 128)))  # Color gris para las calles
+        painter.setBrush(QBrush(Qt.gray))  # Color gris para las calles
         painter.drawRect(200, -10, 100, 510)  # Calle vertical
         painter.drawRect(-10, 200, 510, 100) # Calle horizontal
 
     def drawSemaforo(self, painter):
-        painter.setPen(Qt.NoPen)
         # Dibujar los semáforos
-    
+        painter.setBrush(QBrush(Qt.black)) 
+        painter.drawRect(130, 210, 60, 20)
+        painter.drawRect(210, 315, 20, 60)
+
+        painter.setBrush(QBrush(Qt.gray))
+        # Dibujamos las luces de los semaforos
+        painter.drawEllipse(175, 214, 10, 10)
+        painter.drawEllipse(155, 214, 10, 10)
+        painter.drawEllipse(135, 214, 10, 10)
+
+        painter.drawEllipse(215, 320, 10, 10)
+        painter.drawEllipse(215, 340, 10, 10)
+        painter.drawEllipse(215, 360, 10, 10)
+
+
